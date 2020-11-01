4e8cd41a77975f8aff45808d64ab9380ea01ea67
lironshabtai <liron.shabtai@ex.co>
2020-11-01 15:41:51 +0200

    init configuration

--- eraseDb.js
+++ eraseDb.js
@@ -4,24 +4,16 @@ mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 mongoose.connection.on('open',async (ref) => {
     console.log('Connected to mongo server.');
     collections = await mongoose.connection.db.collections()
-    await collections.forEach(collection => {
+    await Promise.all(collections.map(collection => {
         try {
-            collection.remove();
-            console.log(`removed ${collection.collectionName} succesufully`)
+          return collection.remove();
         } catch(err) {
             console.log(`failed to drop collection: ${collection}, with error: ${err}`)
         }
-    }); 
-    await collections.forEach(collection => {
-        try {
-            collection.remove();
-            console.log(`removed ${collection.collectionName} succesufully`)
-        } catch(err) {
-            console.log(`failed to drop collection: ${collection}, with error: ${err}`)
-        }
-    }); 
+    })); 
     mongoose.disconnect()
     try {
+      await exec(`npmx pm2 restart content`)
       await exec(`npm run populate-db`)
     } catch(err) {
       console.log(`failed to insert data with error: ${err}`)
